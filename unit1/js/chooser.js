9f728c7af5858258c6ad17cea375b332774e1499
dr-maguigan <104376156+dr-maguigan@users.noreply.github.com>
2023-01-26 10:09:59 -0600

    Update chooser.js

--- unit1/js/chooser.js
+++ unit1/js/chooser.js
@@ -1,24 +1,24 @@
 const gameConfig = [ //start game configuration
   //all options for prompt, choice, and options
   {
-    choice: "Moving on",
+    choice: "Start",
     prompt: "Which of the following do you think might be the best example of someone using the Scientific Method/EDP in their daily lives?",
     options: ["Their car won't start", "A light went out", "They have a headache"]
   },
   {
     choice: "Their car won't start",
     prompt: "That's a good one! It could be something with the battery or the alternator so they have to try to figure out what the issue is.",
-    options: ["Moving on"]
+    options: ["Start"]
   },
   {
     choice: "A light went out",
     prompt: "Good one! They have to figure out what is causing the issue. Is it the light bulb, a fuse/circuit breaker, or did the whole area lose power?",
-    options: ["Moving on"]
+    options: ["Start"]
   },
   {
     choice: "They have a headache",
     prompt: "Right! It could be from too much noise/light, they're dehydrated, or something else",
-    options: ["Moving on"]
+    options: ["Start"]
   }
 ];
 
